e28c5b53291e862764584165fc579dc6ac1eba14
Korcsmáros Kende András <f26xc0@INF.ELTE.HU>
2023-11-07 16:26:11 +0100

    kesz

---
+++ Program.cs
@@ -0,0 +1,88 @@
+﻿namespace weather
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //bemenet
+            int dayCount;
+            int raceDays;
+
+            //kimenet
+            bool conditionsOK;
+
+            //ellenorzeshez
+            string inputText;
+            bool error;
+
+            //belovasas
+            do
+            {
+                Console.WriteLine("napok szama(nyilvan nincs negativ nap): ");
+                inputText = Console.ReadLine();
+                error = !int.TryParse(inputText, out dayCount) && dayCount < 0;
+                if(error)
+                {
+                    Console.WriteLine("hogy mennyi, az szam es negativ szamu nap nincs. ujra");
+                }
+            } while (error);
+
+            do
+            {
+                Console.WriteLine("versenynapok szama(nyilvan nincs negativ nap): ");
+                inputText = Console.ReadLine();
+                error = !int.TryParse(inputText, out raceDays) && raceDays < 0;
+                if (error)
+                {
+                    Console.WriteLine("hogy mennyi, az szam es negativ szamu nap nincs. ujra");
+                }
+            } while (error);
+
+            //tomb deklaralas
+
+            Console.WriteLine("sebessegek(nyilvan nincs negativ): ");
+            int[] windSpeeds = new int[dayCount + 1];
+            for (int i = 1; i < dayCount + 1; i++)
+            {
+                do
+                {
+                    
+                    inputText = Console.ReadLine();
+                    error = !int.TryParse(inputText, out windSpeeds[i]) && windSpeeds[i] > 0;
+                    if (error)
+                    {
+                        Console.WriteLine("hogy mennyi, az szam es negativ sebesseg nincs. ujra");
+                    }
+                } while (error);
+            }
+
+            //feldolgozas
+            int suitableDays = 0;
+            for (int i = 0;i <= dayCount; i++)
+            {
+                if (15 <= windSpeeds[i] && windSpeeds[i] <= 35)
+                {
+                    suitableDays++;
+                }
+            }
+
+            conditionsOK = false;
+            if (suitableDays >= raceDays)
+            {
+                conditionsOK = true;
+            }
+
+            //kiir
+            if (conditionsOK)
+            {
+                Console.WriteLine("megrendezheto");
+            }
+            else
+            {
+                Console.WriteLine("nem rendezheto meg");
+            }
+
+        }
+    }
+}
